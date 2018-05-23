3d18c47534e91148f61016a8175b8d6a5b301935
Mattias Buelens <mattias@buelens.com>
2018-05-23 23:25:45 +0200

    Fix indentation

--- src/internal/Request.ts
+++ src/internal/Request.ts
@@ -13,49 +13,49 @@ function normalizeMethod(method: string): string {
 
 class Request extends Body {
 
-constructor(input?: Request | string, options?: RequestInit) {
-  super();
-  options = options || {}
-  var body = options.body
+  constructor(input?: Request | string, options?: RequestInit) {
+    super();
+    options = options || {}
+    var body = options.body
 
-  if (input instanceof Request) {
-    if (input.bodyUsed) {
-      throw new TypeError('Already read')
+    if (input instanceof Request) {
+      if (input.bodyUsed) {
+        throw new TypeError('Already read')
+      }
+      this.url = input.url
+      this.credentials = input.credentials
+      if (!options.headers) {
+        this.headers = new Headers(input.headers)
+      }
+      this.method = input.method
+      this.mode = input.mode
+      this.signal = input.signal
+      if (!body && input._bodyInit != null) {
+        body = input._bodyInit
+        input.bodyUsed = true
+      }
+    } else {
+      this.url = String(input)
     }
-    this.url = input.url
-    this.credentials = input.credentials
-    if (!options.headers) {
-      this.headers = new Headers(input.headers)
+
+    this.credentials = options.credentials || this.credentials || 'omit'
+    if (options.headers || !this.headers) {
+      this.headers = new Headers(options.headers)
     }
-    this.method = input.method
-    this.mode = input.mode
-    this.signal = input.signal
-    if (!body && input._bodyInit != null) {
-      body = input._bodyInit
-      input.bodyUsed = true
+    this.method = normalizeMethod(options.method || this.method || 'GET')
+    this.mode = options.mode || this.mode || null
+    this.signal = options.signal || this.signal
+    this.referrer = null
+
+    if ((this.method === 'GET' || this.method === 'HEAD') && body) {
+      throw new TypeError('Body not allowed for GET or HEAD requests')
     }
-  } else {
-    this.url = String(input)
+    this._initBody(body)
   }
 
-  this.credentials = options.credentials || this.credentials || 'omit'
-  if (options.headers || !this.headers) {
-    this.headers = new Headers(options.headers)
+  clone() {
+    return new Request(this, { body: this._bodyInit })
   }
-  this.method = normalizeMethod(options.method || this.method || 'GET')
-  this.mode = options.mode || this.mode || null
-  this.signal = options.signal || this.signal
-  this.referrer = null
-
-  if ((this.method === 'GET' || this.method === 'HEAD') && body) {
-    throw new TypeError('Body not allowed for GET or HEAD requests')
-  }
-  this._initBody(body)
-}
-
-clone() {
-  return new Request(this, { body: this._bodyInit })
-}
 
 }
 
