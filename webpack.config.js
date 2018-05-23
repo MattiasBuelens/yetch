6d2d327322cee5acb48692236d57871e4bff69d9
Mattias Buelens <mattias@buelens.com>
2018-05-23 22:34:25 +0200

    Set up TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,21 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: ['./src/index.js', './polyfill.js'],
+  entry: ['./src/index.ts', './polyfill.js'],
   output: {
     filename: 'yetch-polyfill.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'Yetch',
     libraryTarget: 'umd',
     globalObject: 'this'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'ts-loader'
+    }]
   }
 };
