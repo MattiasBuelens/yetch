a00534b63d0071e4c19ca597af6056d17a3d2ea5
Mattias Buelens <mattias@buelens.com>
2018-05-24 01:04:59 +0200

    Move polyfill entry point

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,8 +1,8 @@
-var root = require('./src/internal/root').root
-var fetch = require('./src/internal/fetch').fetch
-var Headers = require('./src/internal/Headers').Headers
-var Request = require('./src/internal/Request').Request
-var Response = require('./src/internal/Response').Response
+var root = require('./internal/root').root
+var fetch = require('./internal/fetch').fetch
+var Headers = require('./internal/Headers').Headers
+var Request = require('./internal/Request').Request
+var Response = require('./internal/Response').Response
 var AbortController = require('abortcontroller-polyfill/dist/cjs-ponyfill').AbortController
 var AbortSignal = require('abortcontroller-polyfill/dist/cjs-ponyfill').AbortSignal
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: ['./src/index.ts', './polyfill.js'],
+  entry: ['./src/index.ts', './src/polyfill.ts'],
   output: {
     filename: 'yetch-polyfill.js',
     path: path.resolve(__dirname, 'dist'),
