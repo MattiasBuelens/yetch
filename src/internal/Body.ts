c423b10710ad54470550422ee2173ff6cff4d3eb
Mattias Buelens <mattias.buelens@theoplayer.com>
2018-05-25 18:12:32 +0200

    Move TypedArray to util

--- src/internal/Body.ts
+++ src/internal/Body.ts
@@ -1,16 +1,6 @@
 import { support } from './support'
 import { Headers } from './Headers'
-
-export type TypedArray
-  = Int8Array
-  | Int16Array
-  | Int32Array
-  | Uint8Array
-  | Uint16Array
-  | Uint32Array
-  | Uint8ClampedArray
-  | Float32Array
-  | Float64Array
+import { TypedArray } from './util'
 
 export type BodyInit
   = Blob
