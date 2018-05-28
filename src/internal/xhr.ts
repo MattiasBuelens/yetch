687a8765d9f9e285c99c1a9442763c2e86be2383
Mattias Buelens <mattias.buelens@theoplayer.com>
2018-05-28 18:10:43 +0200

    Refactor XHR

--- src/internal/xhr.ts
+++ src/internal/xhr.ts
@@ -3,6 +3,7 @@ import { DOMException } from './DOMException'
 import { Headers } from './Headers'
 import { Request, RequestInit } from './Request'
 import { Response, ResponseInit } from './Response'
+import { BodyInit } from './Body'
 
 function parseHeaders(rawHeaders: string): Headers {
   const headers = new Headers()
@@ -20,62 +21,146 @@ function parseHeaders(rawHeaders: string): Headers {
   return headers
 }
 
-export function xhrFetch(input: Request | string, init?: RequestInit): Promise<Response> {
-  return new Promise<Response>(function(resolve, reject) {
-    const request = new Request(input, init)
+interface XhrOptions {
+  body: BodyInit
+  headers: Headers
+  method: string
+  responseType: XMLHttpRequestResponseType
+  url: string
+  signal: AbortSignal
+  withCredentials: boolean
+}
 
-    if (request.signal.aborted) {
-      return reject(new DOMException('Aborted', 'AbortError'))
+abstract class Xhr {
+
+  protected readonly _request: Request
+  protected readonly _xhr: XMLHttpRequest
+  private _promise: Promise<Response>
+  protected _resolve!: (response: Response) => void
+  protected _reject!: (reason: any) => void
+
+  protected constructor(request: Request) {
+    this._request = request
+    this._xhr = new XMLHttpRequest()
+    this._promise = new Promise<Response>((resolve, reject) => {
+      this._resolve = resolve
+      this._reject = reject
+    })
+  }
+
+  send(): Promise<Response> {
+    return Promise.resolve(this._getXhrOptions())
+      .then((options) => {
+        this._send(options)
+        return this._promise
+      })
+  }
+
+  protected _getXhrOptions(): XhrOptions | Promise<XhrOptions> {
+    const responseType: XMLHttpRequestResponseType = support.blob ? 'blob' : ''
+    const withCredentials = this._request.credentials === 'include'
+    return {
+      body: this._request._bodyInit,
+      headers: this._request.headers,
+      method: this._request.method,
+      responseType: responseType,
+      url: this._request.url,
+      signal: this._request.signal,
+      withCredentials: withCredentials
     }
+  }
 
-    const xhr = new XMLHttpRequest()
+  protected abstract _onHeadersReceived(init: ResponseInit): void;
 
-    const abortXhr = () => {
-      xhr.abort()
-    }
+  protected abstract _onLoad(): void;
 
-    xhr.onload = () => {
-      const headers = parseHeaders(xhr.getAllResponseHeaders() || '')
-      const options: ResponseInit = {
-        status: xhr.status,
-        statusText: xhr.statusText,
-        headers: headers,
-        url: xhr.responseURL || headers.get('X-Request-URL')
+  protected _onProgress(): void {
+    return
+  }
+
+  protected _onError(): void {
+    this._reject(new TypeError('Network request failed'))
+  }
+
+  protected _onTimeout(): void {
+    this._reject(new TypeError('Network request failed'))
+  }
+
+  protected _onAbort(): void {
+    this._reject(new DOMException('Aborted', 'AbortError'))
+  }
+
+  private _send(options: XhrOptions) {
+    const xhr = this._xhr
+    const abortXhr = () => this._abort()
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === xhr.HEADERS_RECEIVED) {
+        const headers = parseHeaders(xhr.getAllResponseHeaders() || '')
+        const options: ResponseInit = {
+          status: xhr.status,
+          statusText: xhr.statusText,
+          headers: headers,
+          url: xhr.responseURL || headers.get('X-Request-URL')
+        }
+        this._onHeadersReceived(options)
       }
-      const body = xhr.response || xhr.responseText
-      resolve(new Response(body, options))
     }
 
-    xhr.onerror = () => {
-      reject(new TypeError('Network request failed'))
+    xhr.onload = () => this._onLoad()
+    xhr.onerror = () => this._onError()
+    xhr.ontimeout = () => this._onTimeout()
+    xhr.onabort = () => this._onAbort()
+
+    xhr.open(options.method, options.url, true)
+
+    xhr.withCredentials = options.withCredentials
+
+    if ('responseType' in xhr && options.responseType) {
+      xhr.responseType = options.responseType
     }
 
-    xhr.ontimeout = () => {
-      reject(new TypeError('Network request failed'))
-    }
-
-    xhr.onabort = () => {
-      reject(new DOMException('Aborted', 'AbortError'))
-    }
-
-    xhr.open(request.method, request.url, true)
-
-    xhr.withCredentials = (request.credentials === 'include')
-
-    if ('responseType' in xhr && support.blob) {
-      xhr.responseType = 'blob'
-    }
-
-    request.headers.forEach((value, name) => {
+    options.headers.forEach((value, name) => {
       xhr.setRequestHeader(name, value)
     })
 
-    request.signal.addEventListener('abort', abortXhr)
+    options.signal.addEventListener('abort', abortXhr)
 
     xhr.onloadend = () => {
-      request.signal!.removeEventListener('abort', abortXhr)
+      options.signal.removeEventListener('abort', abortXhr)
     }
 
-    xhr.send(request._bodyInit === undefined ? null : request._bodyInit)
+    xhr.send(options.body || null)
+  }
+
+  protected _abort() {
+    this._xhr.abort()
+  }
+
+}
+
+class FetchXhr extends Xhr {
+
+  private _responseInit?: ResponseInit = undefined
+
+  constructor(request: Request) {
+    super(request)
+  }
+
+  protected _onHeadersReceived(init: ResponseInit): void {
+    this._responseInit = init
+  }
+
+  protected _onLoad(): void {
+    const body = this._xhr.response || this._xhr.responseText
+    this._resolve(new Response(body, this._responseInit!))
+  }
+
+}
+
+export function xhrFetch(input: Request | string, init?: RequestInit): Promise<Response> {
+  return new Promise<Response>(resolve => {
+    const request = new Request(input, init)
+    resolve(new FetchXhr(request).send())
   })
 }
