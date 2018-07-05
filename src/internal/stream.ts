838e025fe9e843b4407f0d2c0a424c1bf2589bd3
Mattias Buelens <mattias.buelens@theoplayer.com>
2018-07-05 20:00:27 +0200

    Simplify

--- src/internal/stream.ts
+++ src/internal/stream.ts
@@ -1,7 +1,5 @@
 import {ReadableStream as WhatWGReadableStream} from 'whatwg-streams'
 import {root} from './root'
 
-type GlobalReadableStream<R = Uint8Array> = WhatWGReadableStream<R>
-const GlobalReadableStream: typeof WhatWGReadableStream = root.ReadableStream! as any
-
-export {GlobalReadableStream as ReadableStream}
+export type ReadableStream<R = Uint8Array> = WhatWGReadableStream<R>
+export const ReadableStream: typeof WhatWGReadableStream = root.ReadableStream! as any
