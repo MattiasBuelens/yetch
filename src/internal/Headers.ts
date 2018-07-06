d79b7382f4db2224a15e05b280278b505782583f
Mattias Buelens <mattias.buelens@theoplayer.com>
2018-07-06 11:38:39 +0200

    Remove redundant escape

--- src/internal/Headers.ts
+++ src/internal/Headers.ts
@@ -4,7 +4,7 @@ function normalizeName(name: any): string {
   if (typeof name !== 'string') {
     name = String(name)
   }
-  if (/[^a-z0-9\-#$%&'*+.\^_`|~]/i.test(name)) {
+  if (/[^a-z0-9\-#$%&'*+.^_`|~]/i.test(name)) {
     throw new TypeError('Invalid character in header field name')
   }
   return name.toLowerCase()
